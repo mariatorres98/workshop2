f788b2c510af18d762b73dc98309c4d85fd1e090
Maria Torres <mariale9812@gmail.com>
2022-10-24 12:18:29 -0500

    img

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import '../home/home.scss';
+import fondo from '../../asset/background-crew-desktop.jpg'
 
 
 import {useNavigate, Link} from 'react-router-dom'
@@ -13,9 +15,25 @@ const Home = () => {
   }
   return (
     <>
-      <div className="">
+      <div class="hero-container">
+            <div class="hero-img"> 
+            <img src={fondo} alt="" /></div>
 
-      </div>
+            <div class="space">
+                <div class="space-desc">
+                    <span>so, you want to travel to</span>
+                    <h1>space</h1>
+                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not
+                        hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of
+                        this
+                        world experience!
+                    </p>
+                </div>
+
+                <a class="large-btn" href="">explore</a>
+            </div>
+
+        </div>
     </>
   );
 };
